49564b1ff2d198885c26539b26eb7e40371812a6
Naveen <navaneethk002@gmail.com>
2022-07-11 15:37:03 -0500

    Added new comments and cleaned up the class.
    Added comments and removed some old comments

--- layers.py
+++ layers.py
@@ -1,17 +1,12 @@
-# Custom L1 Distance layer module
-# WHY DO WE NEED THIS: its needed to load the custom model
-
 # Import dependencies
 import tensorflow as tf
 from tensorflow.keras.layers import Layer
 
-# Custom L1 Distance Layer from Jupyter
+# A copy of the custom L1 Dist layer used to create the model.
 class L1Dist(Layer):
-
     # Init method - inheritance
     def __init__(self, **kwargs):
         super().__init__()
 
-    # Magic happens here - similarity calculation
     def call(self, input_embedding, validation_embedding):
         return tf.math.abs(input_embedding - validation_embedding)
